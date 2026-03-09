11d3e9b227ee2bdcb7bb38c2774a0ef98eb75171
Mariannly <mariannly.merquez@gmail.com>
2026-03-09 17:15:38 -0500

    rules

---
+++ src/rules.py
@@ -0,0 +1,23 @@
+# Deterministic Rules
+
+def autocatalysis():
+    """
+    """
+
+def mutual_inhibition():
+    """
+    """
+
+# Stochastic Rules
+
+def spontaneus_neutrality():
+    """
+    """
+
+def spontaneous_chirality():
+    """
+    """
+
+def diffusion():
+    """
+    """
