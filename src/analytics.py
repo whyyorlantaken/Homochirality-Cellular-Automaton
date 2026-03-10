2766605d674e2bb2dc35a7fd0f030b4baffb37d7
Mariannly <mariannly.merquez@gmail.com>
2026-03-09 21:22:43 -0500

    plotting structure

---
+++ src/analytics.py
@@ -0,0 +1,1 @@
+# analysis of clustering hereee
---
+++ src/plotting.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def chiralPopulationPlot(chiralPop):
+    """
+    Populations over time
+    """
+
+def chaosParameterPlot():
+    """
+    Populations over epsilon
+    """
+
+def phaseSpacePlot():
+    """
+    Phase space plot
+    """
+
+def spatialPlot():
+    """
+    Spatial plot
+    """
+
+def spatialgif():
+    """
+    Spatial gif
+    """
+
